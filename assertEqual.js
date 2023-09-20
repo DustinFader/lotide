40741da85e3f9cf39d04273f21703e6c2709659e
DustinFader <dustinfader16@email.com>
2023-09-20 20:07:14 -0300

    refactered concat string to literal string

--- assertEqual.js
+++ assertEqual.js
@@ -1,7 +1,7 @@
 const assertEqual = (actual, expected) => {
   if (actual === expected) {
-    console.log("❤✅❤ Assertion Passed: " + actual + ' === ' + expected);
-  } else console.log("🔴🤔🔴 Assertion Failed: " + actual + ' !== ' + expected);
+    console.log(`❤✅❤ Assertion Passed: ${actual} === ${expected}`);
+  } else console.log(`🔴🤔🔴 Assertion Failed: ${actual} !== ${expected}`);
 };
 
 // Test code
