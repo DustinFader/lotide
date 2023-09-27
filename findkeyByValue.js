5d6397d4601371fffac0a2f26b16b9f3b019eb83
DustinFader <dustinfader16@gmail.com>
2023-09-27 11:57:31 -0300

    created a function that finds what genre in a list of genres and thier best shows is when givin a show. If show isnt in any genres than returned undefined.

---
+++ findkeyByValue.js
@@ -0,0 +1,25 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`❤✅❤ Assertion Passed: ${actual} === ${expected}`);
+  } else console.log(`🔴🤔🔴 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const findKeyByValue = (bestByGenre, show) => {
+  for (let item of Object.keys(bestByGenre)) {
+    if (bestByGenre[item] === show) {
+      return item;
+    }
+  }
+};
+
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
+assertEqual(findKeyByValue(bestTVShowsByGenre, ""), undefined);
