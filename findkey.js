0c2b7fd0e5c0c405b8affc7649fc1c08c61cd806
DustinFader <dustinfader16@gmail.com>
2023-09-30 16:34:35 -0300

    made a function that finds a key in object if any keys value returns true when tested by the callback

---
+++ findkey.js
@@ -0,0 +1,26 @@
+const findKey = (object, callback) => {
+  for (let key of Object.keys(object)) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+};
+
+let test = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "Alohora":   { stars: 2 },
+  "Kudinshaw": { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2); // => "Alohora"
+
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`❤✅❤ Assertion Passed: ${actual} === ${expected}`);
+  } else console.log(`🔴🤔🔴 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+assertEqual(test, "Alohora");
+assertEqual(findKey({path: "court", pinch: "jork", mort: "fort"}, x => x === "port"), undefined);
+assertEqual(findKey({path: "court", pinch: "jork", mort: "fort"}, x => x === "jork"), "pinch");
