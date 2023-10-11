d4a3ec05e0234d4bc820c75c226b325574721bee
DustinFader <dustinfader16@gmail.com>
2023-10-11 15:53:10 -0300

    created new package files and added a mocha and chai library. Refactered headtest.js to use new libraries.

--- test/headTest.js
+++ test/headTest.js
@@ -1,9 +1,19 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const head = require('../head');
 
 // head test cases
-console.log("\n-- head test cases --");
-assertEqual(head([1, 2, 3]), 1);
-assertEqual(head(["bad", "good", "neutral", "Happenstance"]), "bad");
-assertEqual(head([, 0]),); //eslint may say this is wrong but i wanted to test it this way anyways.
-assertEqual(head([0]), 0);
+// console.log("\n-- head test cases --");
+// assertEqual(head([1, 2, 3]), 1);
+// assertEqual(head(["bad", "good", "neutral", "Happenstance"]), "bad");
+// assertEqual(head([, 0]),); //eslint may say this is wrong but i wanted to test it this way anyways.
+// assertEqual(head([0]), 0);
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it('returns "5" for ["5"]', () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+});
