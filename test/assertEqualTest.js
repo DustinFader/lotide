c0f3dc56aa85c476d175fb8ed6ab9e5b42f5210b
DustinFader <dustinfader16@gmail.com>
2023-10-11 13:24:55 -0300

    -

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -2,7 +2,7 @@ const assertEqual = require('../assertEqual')
 const head = require('../head')
 
 // assertEqual Test code
-console.log("-- assertEqual Test code --")
+console.log("\n-- assertEqual Test code --")
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 assertEqual("", "");
@@ -13,7 +13,7 @@ assertEqual([1, 2], [2, 1]);
 assertEqual([1, 2], [1, 2]);
 
 //head test cases
-console.log("-- head test cases --")
+console.log("\n-- head test cases --")
 assertEqual(head([1, 2, 3]), 1);
 assertEqual(head(["bad", "good", "neutral", "Happenstance"]), "bad");
 assertEqual(head([, 0]),); //eslint may say this is wrong but i wanted to test it this way anyways.
