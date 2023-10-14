7678d079dbb98b4deb73276d874b1541ed3d0e6d
DustinFader <dustinfader16@gmail.com>
2023-10-14 01:41:39 -0300

    refactered eqArray to take in nested arrays through now and added more tests to its test file.

--- eqArrays.js
+++ eqArrays.js
@@ -1,6 +1,17 @@
 // checks if two arrays equal the same. True if they are.
 const eqArrays = (array1, array2) => {
+  if (array1.length !== array2.length) { // if lengths arent the same then not equal
+    return false;
+  }
   for (let i = 0; i < array1.length; i++) {
+    if (Array.isArray(array1[i])) {
+      // if callback is true then continues to next item in array
+      if (!eqArrays(array1[i], array2[i])) {
+        return false;
+      }
+      continue;
+    }
+    // checks if base value is the same
     if (array1[i] !== array2[i]) {
       return false;
     }
--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -18,5 +18,28 @@ describe("#eqArrays", () => {
   it('returns false if both arrays dont equal each other', () => {
     assert.strictEqual(_.eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
   });
-});
 
+  it('returns true if both nested arrays equal each other', () => {
+    assert.strictEqual(_.eqArrays([2, [2, [2, 4]]], [2, [2, [2, 4]]]), true);
+  });
+
+  it('returns true if both nested arrays equal each other', () => {
+    assert.strictEqual(_.eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
+  });
+
+  it('returns false since value and array dont equal each other', () => {
+    assert.strictEqual(_.eqArrays(1, [1]), false);
+  });
+
+  it('returns false since both arrays dont equal each other', () => {
+    assert.strictEqual(_.eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
+  });
+
+  it('returns false since both arrays dont equal each other', () => {
+    assert.strictEqual(_.eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
+  });
+
+  it('returns true since both arrays equal each other', () => {
+    assert.strictEqual(_.eqArrays([[2, 3], ["try"]], [[2, 3], ["try"]]), true);
+  });
+});
